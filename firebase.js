06dca5c2a707ef2bae66987a643f540339b50920
gk942 <gkhanhluv@gmail.com>
2025-06-26 22:42:17 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,10 @@
+<!-- Firebase SDK (dùng CDN để hỗ trợ trên GitHub Pages) -->
 <script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js"></script>
 <script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-database-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-auth-compat.js"></script>
 
 <script>
+  // Cấu hình Firebase thực tế
   const firebaseConfig = {
     apiKey: "AIzaSyDBaWDDCkhRa3DQcJBYdznL98GjHmXspuI",
     authDomain: "gkdmshop.firebaseapp.com",
@@ -9,9 +12,14 @@
     storageBucket: "gkdmshop.appspot.com",
     messagingSenderId: "52437838526",
     appId: "1:52437838526:web:3187795557aa1458190cef",
-    measurementId: "G-V4QFPWK82L"
+    measurementId: "G-V4QFPWK82L",
+    databaseURL: "https://gkdmshop-default-rtdb.firebaseio.com"
   };
 
+  // Khởi tạo Firebase
   firebase.initializeApp(firebaseConfig);
+
+  // Khởi tạo Database và Auth
   const database = firebase.database();
+  const auth = firebase.auth();
 </script>
