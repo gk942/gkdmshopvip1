3e29191f99802c0bdd849b7de700fcd127ac8957
gk942 <gkhanhluv@gmail.com>
2025-06-27 21:31:54 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
-<!-- firebase.js -->
+<!-- Firebase SDK compat -->
 <script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-auth-compat.js"></script>
 <script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-database-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-auth-compat.js"></script>
 
 <script>
   const firebaseConfig = {
