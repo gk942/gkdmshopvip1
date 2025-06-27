89994a0c92e04139dfacb2c98685086868d0ea57
gk942 <gkhanhluv@gmail.com>
2025-06-27 17:23:17 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,9 @@
-<!-- firebase.js -->
-<!-- Bạn có thể dùng file riêng hoặc gắn trong thẻ <script> -->
-
-<!-- Nhúng Firebase SDK -->
+<!-- Đặt vào đầu <head> -->
 <script type="module">
-  // Import SDK dạng module
+  // Import SDK từ Firebase
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-  import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+  import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+  import { getDatabase, ref, set, push, onValue, update } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
   // Cấu hình Firebase
   const firebaseConfig = {
@@ -14,7 +11,7 @@
     authDomain: "gkdmshop.firebaseapp.com",
     databaseURL: "https://gkdmshop-default-rtdb.firebaseio.com",
     projectId: "gkdmshop",
-    storageBucket: "gkdmshop.appspot.com", // Sửa lại tên bucket cho đúng
+    storageBucket: "gkdmshop.appspot.com",
     messagingSenderId: "52437838526",
     appId: "1:52437838526:web:3187795557aa1458190cef",
     measurementId: "G-V4QFPWK82L"
@@ -22,11 +19,13 @@
 
   // Khởi tạo Firebase
   const app = initializeApp(firebaseConfig);
-  const database = getDatabase(app);
-  const auth = getAuth(app);
-
-  // Đưa ra global (để dùng ở file HTML khác nếu cần)
-  window.firebaseApp = app;
-  window.database = database;
-  window.auth = auth;
+  window.auth = getAuth(app);
+  window.database = getDatabase(app);
+  window.ref = ref;
+  window.set = set;
+  window.push = push;
+  window.onValue = onValue;
+  window.update = update;
+  window.signInWithEmailAndPassword = signInWithEmailAndPassword;
+  window.createUserWithEmailAndPassword = createUserWithEmailAndPassword;
 </script>
