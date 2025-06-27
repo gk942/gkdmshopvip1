a53c588e0ef885aafe59f5b3fdc80c0eb07decb0
gk942 <gkhanhluv@gmail.com>
2025-06-27 21:03:43 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,24 @@
-// firebase.js
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "gkdmshop.firebaseapp.com",
-  databaseURL: "https://gkdmshop-default-rtdb.firebaseio.com",
-  projectId: "gkdmshop",
-  storageBucket: "gkdmshop.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+<!-- Firebase SDK dạng compat (phù hợp GitHub Pages) -->
+<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-auth-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-database-compat.js"></script>
 
-firebase.initializeApp(firebaseConfig);
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyDBaWDDCkhRa3DQcJBYdznL98GjHmXspuI",
+    authDomain: "gkdmshop.firebaseapp.com",
+    databaseURL: "https://gkdmshop-default-rtdb.firebaseio.com",
+    projectId: "gkdmshop",
+    storageBucket: "gkdmshop.appspot.com",
+    messagingSenderId: "52437838526",
+    appId: "1:52437838526:web:3187795557aa1458190cef",
+    measurementId: "G-V4QFPWK82L"
+  };
+
+  // Khởi tạo Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // Dùng biến auth và database ở mọi trang
+  const auth = firebase.auth();
+  const database = firebase.database();
+</script>
