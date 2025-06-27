34ab47b2ef282057813395eefec1aa95fc61b1cd
gk942 <gkhanhluv@gmail.com>
2025-06-27 20:40:12 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,12 @@
-<!-- firebase.js -->
-<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-database-compat.js"></script>
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyDBaWDDCkhRa3DQcJBYdznL98GjHmXspuI",
-    authDomain: "gkdmshop.firebaseapp.com",
-    databaseURL: "https://gkdmshop-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "gkdmshop",
-    storageBucket: "gkdmshop.appspot.com",
-    messagingSenderId: "52437838526",
-    appId: "1:52437838526:web:3187795557aa1458190cef",
-    measurementId: "G-V4QFPWK82L"
-  };
-  firebase.initializeApp(firebaseConfig);
-</script>
+// firebase.js
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "gkdmshop.firebaseapp.com",
+  databaseURL: "https://gkdmshop-default-rtdb.firebaseio.com",
+  projectId: "gkdmshop",
+  storageBucket: "gkdmshop.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
