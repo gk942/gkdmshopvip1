81fd82526bc64eb41fe7892de9daa958196bc7e5
gk942 <gkhanhluv@gmail.com>
2025-06-27 12:41:11 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,16 @@
-<!-- firebase.js -->
-<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-database-compat.js"></script>
+<<!-- firebase.js -->
+<script src="https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.22.2/firebase-database-compat.js"></script>
 
 <script>
   const firebaseConfig = {
     apiKey: "AIzaSyDBaWDDCkhRa3DQcJBYdznL98GjHmXspuI",
     authDomain: "gkdmshop.firebaseapp.com",
-    projectId: "gkdmshop",
     databaseURL: "https://gkdmshop-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "gkdmshop",
     storageBucket: "gkdmshop.appspot.com",
     messagingSenderId: "52437838526",
-    appId: "1:52437838526:web:3187795557aa1458190cef",
-    measurementId: "G-V4QFPWK82L"
+    appId: "1:52437838526:web:3187795557aa1458190cef"
   };
 
   firebase.initializeApp(firebaseConfig);
