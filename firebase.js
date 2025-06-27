00ffcbcfd000a960a4eeaf76fff55e1d261992fd
gk942 <gkhanhluv@gmail.com>
2025-06-27 20:02:16 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,7 @@
 <!-- firebase.js -->
-<!-- Firebase SDK Compat -->
 <script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js"></script>
 <script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-auth-compat.js"></script>
 <script src="https://www.gstatic.com/firebasejs/9.22.0/firebase-database-compat.js"></script>
-
 <script>
   const firebaseConfig = {
     apiKey: "AIzaSyDBaWDDCkhRa3DQcJBYdznL98GjHmXspuI",
@@ -15,8 +13,5 @@
     appId: "1:52437838526:web:3187795557aa1458190cef",
     measurementId: "G-V4QFPWK82L"
   };
-
   firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-  const database = firebase.database();
 </script>
